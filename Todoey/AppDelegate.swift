a6456a9fbaad4ea396deb98977ee7534138968d2
Dachary <dc@dacharycarey.com>
2021-03-04 10:48:26 -0500

    Data persisted using UserDefaults method.

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -14,7 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
+    }
+    
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground.
+        print("applicationWillTerminate")
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
 }
 
